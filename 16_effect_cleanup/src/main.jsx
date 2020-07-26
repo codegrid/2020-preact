9fb4686774b4cfef9c488e515301871e96d1b741
Yuji Sugiura <y.sugiura.0316@gmail.com>
2020-07-26 22:27:19 +0900

    Add 16

---
+++ 16_effect_cleanup/src/main.jsx
@@ -0,0 +1,19 @@
+import { h, render } from "preact";
+import { useState, useEffect } from "preact/hooks";
+
+const App = () => {
+  const [point, setPoint] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const onResize = ({ clientX, clientY }) => setPoint({ x: clientX, y: clientY });
+    window.addEventListener("mousemove", onResize);
+    return () => window.removeEventListener("mousemove", onResize);
+  }, []);
+
+  return (
+    <div>
+      x: {point.x} / y: {point.y}
+    </div>
+  );
+};
+
+render(<App />, document.body);
